c74ea7f2ff5f8e98f132a86c5e663e13fe5d063a
Kartik K Nayak <kartikkumar1966@gmail.com>
2025-07-26 14:17:51 +0530

    v1 of pythonprog1.py

---
+++ pythonprog1.py
@@ -0,0 +1,8 @@
+a = float(input('Enter a number\n'))
+b = float(input('Enter a second number\n'))
+s=a+b
+print('The sum value is',s)
+fd=a/b
+print('The float division value',fd)
+d=a//b
+print('The integer division value is',d)
